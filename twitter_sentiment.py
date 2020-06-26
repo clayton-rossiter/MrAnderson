b3cb257482b60ef84e67f04c2e3759ebdb7ad2fa
cla114 <clayton.rossiter@gmail.com>
2020-06-26 13:52:00 +0100

    Add error capture to search()

--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -2,6 +2,7 @@
 import secret_stuff
 # libraries
 import tweepy as tw
+import re
 
 class MrAnderson():
     def __init__(self):
@@ -31,16 +32,43 @@ class MrAnderson():
                                     filter retweets from results
         outputs:    pd.DataFrame/List of tweets from search terms
         '''
+        # check data types are correct
+        # hashtag
+        if not isinstance(hashtag,str):
+            print("Ensure search term is a string!")
+            return
+        else:
+            if hashtag[0] != '#':
+                self.hashtag = ''.join(('#',hashtag.strip()))
+            else:
+                self.hashtag = hashtag
+        # search_after
+        if not isinstance(search_after,str):
+            print('Ensure search_after is a string!')
+            return
+        else:
+            if not re.match(r'\d{4}-\d{2}-\d{2}', search_after):
+                print('Ensure search_after is in YYYY-MM-DD format!')
+                return
+                
+        # run search function using Cursor
         self.tweets = tw.Cursor(
             self.api.search,
             q = hashtag,
             lang="en",
             since=search_after
         ).items(10)
+    
+    def export(self):
+        '''
+        purpose:    export saved search results to a csv file in current directory
+        inputs:     must have previously run search (error raise)
+        outputs:    results.csv in current
+        '''
 
 
 
 if __name__ == '__main__':
     neo = MrAnderson()
-    neo.search('#blm','2020-06-01')
+    neo.search('blm','2020-06-01')
     
