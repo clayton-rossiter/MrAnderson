bbaf313c521c43aa920bb7da5e7344c551916b7d
cla114 <clayton.rossiter@gmail.com>
2020-06-28 15:25:51 +0100

    NB classifier saved to pickle

--- trainer.py
+++ trainer.py
@@ -7,8 +7,10 @@ import nltk
 from nltk.tokenize import word_tokenize
 from string import punctuation 
 from nltk.corpus import stopwords 
+import pickle
 
-def get_training_data(filename='corpus.csv'):
+
+def download_training_data(filename='corpus.csv'):
     '''
     '''
     df = pd.read_csv('corpus.csv', names=['Handle','Sentiment','ID', 'Tweet'])
@@ -30,17 +32,22 @@ class PreProcessTweets:
     '''
     def __init__(self):
         self._stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
+        self.get_data()
 
     def get_data(self, filename='training.csv'):
         df = pd.read_csv(filename)
-        df = df.dropna()
-        # remove null entries
-        print(df.head())
+        self.trainingSetRaw = df.dropna().head()
         
-    def processTweets(self, list_of_tweets):
+    def processTweets(self):
         processedTweets=[]
-        for tweet in list_of_tweets:
-            processedTweets.append((self._processTweet(tweet["text"]),tweet["label"]))
+        for i, tweet in self.trainingSetRaw.iterrows():
+            processedTweets.append((self._processTweet(tweet["Tweet"]),tweet["Sentiment"]))
+        self.trainingSetProcessed = processedTweets
+
+    def buildTrainingSet(tweets):
+        processedTweets=[]
+        for tweet in tweets:
+            processedTweets.append(self._processTweet(tweet),None)
         return processedTweets
     
     def _processTweet(self, tweet):
@@ -51,6 +58,40 @@ class PreProcessTweets:
         tweet = word_tokenize(tweet) # remove repeated characters (helloooooooo into hello)
         return [word for word in tweet if word not in self._stopwords]
 
+    def buildVocabulary(self):
+        def extract_features(tweet):
+            tweet_words = set(tweet)
+            features = {}
+            for word in self.wordFeatures:
+                features['contains(%s)' % word] = (word in tweet_words)
+            return features 
+
+        all_words = []
+        for (words, sentiment) in self.trainingSetProcessed:
+            all_words.extend(words)
+        self.wordList = nltk.FreqDist(all_words)
+        self.wordFeatures = self.wordList.keys()
+        self.trainingFeatures = nltk.classify.apply_features(extract_features, self.trainingSetProcessed)
+
+    def naiveBayes(self):
+        self.NB = nltk.NaiveBayesClassifier.train(self.trainingFeatures)
+        return self.NB
+
+    def saveModel(self, model, filename='model'):
+        f = open(filename, 'wb')
+        pickle.dump(model, f)
+        f.close()
+
+    def loadModel(self, filename='model'):
+        f = open(filename, 'rb')
+        model = pickle.load(f)
+        f.close()
+        return model
+
 if __name__ == '__main__':
     n = PreProcessTweets()
-    n.get_data()
+    n.processTweets()
+    n.buildVocabulary()
+    model = n.naiveBayes()
+    n.saveModel(model)
+    
